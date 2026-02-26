a86298598983387f4739b4052f6441493b234707
Malik Araf <155768513+malikaraf@users.noreply.github.com>
2026-02-27 03:31:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,9 @@ app = Flask(__name__)
 def haven_command():
     return jsonify({
         "response_type": "in_channel",
-        "text": "Haven is a next gen accounting firm + platform. Bookkeeping, taxes, tax credits, invoicing, bill pay and more for startups.
-
-We are their customers and love them. Wanted to recommend them. If anyone wants an intro please let us know and we will introduce you to their CEO on slack.
-
-Their team are ex EY/KPMG CPAs and they serve 1000+ amazing businesses. Every client has a dedicated CPA and slack channel that their team is available in 24/7, 365.
-
-Their approach is tech-enabled, streamlining the accounting and tax process with powerful automation via their platform, while simultaneously providing the expert human touch.
-Some notable customers include: Sierra, Felix Superpower, Flora, to name a few."
-    })
+        "text": """Haven is a next gen accounting firm + platform. Bookkeeping, taxes, tax credits, invoicing, bill pay and more for startups.\nWe are their customers and love them. Wanted to recommend them. If anyone wants an intro please let us know and we will introduce you to their CEO on slack.\nTheir team are ex EY/KPMG CPAs and they serve 1000+ amazing businesses. Every client has a dedicated CPA and slack channel that their team is available in 24/7, 365.\nTheir approach is tech-enabled, streamlining the accounting and tax process with powerful automation via their platform, while simultaneously providing the expert human touch.\nSome notable customers include: Sierra, Felix Superpower, Flora, to name a few."""
+    }), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 3000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
